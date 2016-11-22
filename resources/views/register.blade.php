3766e43d2851e0f397458c3def4f04d8e2ade504
Jeff <jefferson.g.cosme@gmail.com>
2016-11-23 00:49:36 +0800

    Add footer

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -48,7 +48,7 @@
         <title>SeagrassSpotter</title>
 
         <!-- Fonts -->
-		<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+		<link href="{{{ asset('css/fonts-googleapis.css') }}}" rel="stylesheet" type="text/css">
         <!-- Styles -->
 
 		<link href="{{{ asset('css/bootstrap.min.css') }}}" rel="stylesheet">
@@ -60,43 +60,7 @@
         </style>
     </head>
     <body>
-		<nav class="navbar navbar-default" role="navigation">
-		  <div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-			  <span class="icon-bar"></span>
-			  <span class="icon-bar"></span>
-			  <span class="icon-bar"></span>
-			</button>    
-		  </div>
-		  <div class="navbar-collapse collapse">
-			<ul class="nav navbar-nav navbar-left">
-				<li><a href="#">LOGO</a></li>
-			</ul>
-			<ul class="nav navbar-nav navbar-right">
-				<div class="navbar-header page-scroll">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-				</div>
-
-				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse" id="navbar-collapse">
-					<ul class="nav navbar-nav">
-					   
-					   <li class="active"><a href="#">Home</a></li>
-					   <li><a href="#">About</a></li>
-					   <li><a href="#">Services</a></li>
-					   <li><a href="#">Products</a></li>
-					   <li><a href="#">Contacts</a></li>
-					   <li><a href="#">Blog</a></li>
-					</ul>
-				</div>
-			</ul>
-		  </div>
-		</nav>
+		@include('navbar')
 
 		<div class="container">
 			<div id="navi">
